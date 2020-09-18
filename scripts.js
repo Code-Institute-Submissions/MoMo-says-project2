f0746414439a146d592b32ebcfd39259b0845456
QuelleQim <cathrine.karl@gmail.com>
2020-09-18 14:45:45 +0000

    Add fadeTo to animal image in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,26 @@
 $("document").ready(function() {
 
-    $("#pig").click(function(){ // When I click on <p> run this code });
-        $("#pig").css("border", "solid 7px red")
-        });
-    $("#sealion").click(function(){
-        $("#sealion").css("border", "solid 7px red");
-    $("#rattlesnake").click(function(){ // When I click on <p> run this code });
-        $("#rattlesnake").css("border", "solid 7px red")
-        });
     $("#elephant").click(function(){ // When I click on <p> run this code });
-        $("#elephant").css("border", "solid 7px blue")
-        });        
+        $("#elephant").css("border", "solid 7px red").css("opacity", "0.6");
+    });    
+    $("#pig").click(function(){
+        $("#pig").css("border", "solid 7px green").css("opacity", "0.6");
+    });
+    $("#rattlesnake").click(function(){
+        $("#rattlesnake").fadeOut(1000).fadeIn(1000);
+    });
+    $("#sealion").click(function(){
+        $("#sealion").fadeTo(500, 0.5).fadeTo(500, 1);
     });
 
 });
-
-
-
-
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("#div1").fadeTo("slow", 0.15);
+//     $("#div2").fadeTo("slow", 0.4);
+//     $("#div3").fadeTo("slow", 0.7);
+//   });
+// });
 
 
 
