0f725b01efce034abaee1e045981e42e790ac28e
QuelleQim <cathrine.karl@gmail.com>
2020-09-18 10:10:26 +0000

    Add code to js file

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$("#pig").click(function(){ // When I click on <p> run this code });
+    $("#pig").css("border", "red");
+    console.log("hej")
+});
+
+console.log("Hello")
+
+
+
+
+// $("#pig").mouseenter(function() {
+//   $("pig").removeClass("make-red").addClass("make-border");
+// });
+// $("button").mouseleave(function() {
+//   $("button").removeClass("make-border").addClass("make-red");
+// });
