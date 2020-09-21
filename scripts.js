f1688ffeb713302479c0ca95ec0a675a68b018fe
QuelleQim <cathrine.karl@gmail.com>
2020-09-21 09:12:00 +0000

    Change the structure of index.html to bootstrap for cross platform responsiveness

--- scripts.js
+++ scripts.js
@@ -1,27 +1,26 @@
 $("document").ready(function() {
 
     $("#elephant").click(function(){ // When I click on <p> run this code });
-        $("#elephant").css("border", "solid 7px red").css("opacity", "0.6");
+        $("#elephant").fadeTo(500, 0.4).fadeTo(500, 1);
     });    
     $("#pig").click(function(){
-        $("#pig").css("border", "solid 7px green").css("opacity", "0.6");
+        $("#pig").fadeTo(500, 0.4).fadeTo(500, 1);
     });
     $("#rattlesnake").click(function(){
-        $("#rattlesnake").fadeOut(1000).fadeIn(1000);
+        $("#rattlesnake").fadeTo(500, 0.4).fadeTo(500, 1);
     });
     $("#sealion").click(function(){
-        $("#sealion").fadeTo(500, 0.5).fadeTo(500, 1);
+        $("#sealion").fadeTo(500, 0.4).fadeTo(500, 1);
     });
 
 });
-// $(document).ready(function(){
-//   $("button").click(function(){
-//     $("#div1").fadeTo("slow", 0.15);
-//     $("#div2").fadeTo("slow", 0.4);
-//     $("#div3").fadeTo("slow", 0.7);
-//   });
-// });
+
 
 
 
 console.log("Hello");
+
+
+// start board sequence
+
+// start user replicating sequence
