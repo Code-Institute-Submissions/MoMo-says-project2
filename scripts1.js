794da3761451b8ba2af2845a4e808253f6ccbb71
QuelleQim <cathrine.karl@gmail.com>
2020-10-02 10:00:08 +0000

    Add functioning local storage of highest score

--- scripts1.js
+++ scripts1.js
@@ -13,16 +13,20 @@ var boardSound = [
 ];
 
 var localStorageName = "MoMoSays";
-var localhighScore;
+var localHighScore;
 
-// localhighScore = localStorage.getItem(localStorageName) == null ? 0 :
-//                  localStorage.getItem(localStorageName);
+localHighScore = localStorage.getItem(localStorageName) == null ? 0 :
+                 localStorage.getItem(localStorageName);
+                 $("#highest-highscore").text(localHighScore);
 
-if(localStorage.getItem(localStorageName) == null) {
-    localHighScore = 0;
-} else {
-    localHighScore = localStorage.getItem(localStorageName);
-}
+
+// localStorage.setItem(localHighScore);
+
+// if(localStorage.getItem(localStorageName) == null) {
+//     localHighScore = 0;
+// } else {
+//     localHighScore = localStorage.getItem(localStorageName);
+// }
 
 // const isStorage = "undefined" !== typeof localStorage;
 
@@ -30,53 +34,54 @@ if(localStorage.getItem(localStorageName) == null) {
 let firstClickAudio = true
 
 $(document).ready(function () {
-    $(".animal-panel").click(function() {
-        if (firstClickAudio === true) {
-            firstClickAudio = false
-            momoSequence();
-            return
-        }
-        id = $(this).attr("id");
-        activePanel(id);
-    })
+$(".animal-panel").click(function() {
+    if (firstClickAudio === true) {
+        firstClickAudio = false
+        momoSequence();
+        return
+    }
+    id = $(this).attr("id");
+    activePanel(id);
+})
 });
+
 
 
 /* user pad listener */
 function activePanel(id) {
-    console.log("User: " + id);
-    userSeq.push(id);
-    addClassSound(id);
+console.log("User: " + id);
+userSeq.push(id);
+addClassSound(id);
 
-    /* check user sequence */
-    if(!checkUserSeq()) {
-            displayWrong();
-            userSeq = [];
-        return
-        }
-
-        /* check user length */
-    if(userSeq.length == momoSeq.length && userSeq.length < NUM_OF_SCORE) {
-        score++;
+/* check user sequence */
+if(!checkUserSeq()) {
+        displayWrong();
         userSeq = [];
-        momoSequence();
+    return
     }
 
-    /* checking for winner */
-    if(userSeq.length == NUM_OF_SCORE) {
-        alert("Congratulations, you win!");
-    }
+    /* check user length */
+if(userSeq.length == momoSeq.length && userSeq.length < NUM_OF_SCORE) {
+    score++;
+    userSeq = [];
+    momoSequence();
+}
+
+/* checking for winner */
+if(userSeq.length == NUM_OF_SCORE) {
+    alert("Congratulations, you win!");
+}
 };
 
 
 /* checking user seq agaings momo's */
 function checkUserSeq(i) {
-    for(var i = 0; i < userSeq.length; i++) {
-        if(userSeq[i] != momoSeq[i]) {
-            return false;
-        }
+for(var i = 0; i < userSeq.length; i++) {
+    if(userSeq[i] != momoSeq[i]) {
+        return false;
     }
-    return true
+}
+return true
 }
 
 
@@ -84,16 +89,21 @@ function displayWrong() {
     var wrongAudio = document.getElementById("tarzan");
     console.log("Wrong");
     $("#current-highscore").text("Wrong");
-    $("#highest-highscore").text(localHighScore);
+
+    localHighScore = Math.max(score, localHighScore);
+    localStorage.setItem(localStorageName, localHighScore);
     console.log(localHighScore)
+    $("#highest-highscore").text(localHighScore);
+
+
     userSeq = [];
     momoSeq = [];
     score = 0;
-        if (!mute) {
+    if (!mute) {
         wrongAudio.play();
-        }
+    }
         
-    // isStorage && localStorage.setItem("current-highscore", elements.scores);
+
 }
 
 /* momo sequence */
@@ -116,8 +126,8 @@ function momoSequence() {
 
 /* generate random number */ 
 function getRandomNum() {
-    var random = Math.floor(Math.random() * 4); 
-    momoSeq.push(random);
+var random = Math.floor(Math.random() * 4); 
+momoSeq.push(random);
 }
 
 /* add active panel */
@@ -133,52 +143,42 @@ function addClassSound(id, color) {
 // function() {
 //     highScore = Math.max(score, highScore);
 //     localStorage.setItem(localStorageName, highScore);
+// function setHighScore() {
+//     localHighScore = Math.max(score, localhighScore);
+//     if (score < localhighScore) {
+//         $("#highest-highscore").text(score);
+//     localStorage.setItem(localStorageName, localhighScore);
+//     }
+//     else  {
+//         return
+//     }
 
-	
-function setHighScore() {
-    localHighScore = Math.max(score, localhighScore);
-    localStorage.setItem(localStorageName, localhighScore);
-    // document.getElementById("highest-highscore").innerText = localhighScore;
-       
-    let text = document.getElementById("highest-highscore");
-    text.innerText = localhighScore;
-
-        // game.width / 2, game.height / 2, "Game Over\n\nYour score: " + score + "\nBest score: " + highScore + "\n\nTap to restart", style
+// document.getElementById("highest-highscore").innerText = localhighScore;
     
-    
-    // text.anchor.set(0.5);
-    // game.input.onDown.add(this.restartGame, this);
-}
+// let text = document.getElementById("highest-highscore");
+// text.innerText = localhighScore;
 
 
 
 
-// Code from Styled JavaScript https://www.youtube.com/watch?v=NmXEJIBsN-4
-// if (isStorage && localStorage.getItem("current-highscore")) {
-//     customElements.scores = localStorage.getItem("current-highscore").split(",")
-//     push.("highest-highscore");
-// }
-
-// function highscore() {
-//  if (userSeq > 
-// }
 
 
-// mute button
+
+/* mute button */
 let mute = false
 
 
 function muteAudio() {
-    document.querySelector('.mute-button').style.display = "None";
-    document.querySelector('.enable-audio-button').style.display = "Inline-block";
-    mute = true   
+document.querySelector('.mute-button').style.display = "None";
+document.querySelector('.enable-audio-button').style.display = "Inline-block";
+mute = true   
 }
 
 
 function enableAudio() {
-    document.querySelector('.enable-audio-button').style.display = "None";
-    document.querySelector('.mute-button').style.display = "Inline-block";
-    mute = false   
+document.querySelector('.enable-audio-button').style.display = "None";
+document.querySelector('.mute-button').style.display = "Inline-block";
+mute = false   
 }
 
 
@@ -192,9 +192,10 @@ function playSound(id) {
 
 
 function resetGame() {
-    userSeq = [];
-    momoSeq = [];
-    score = 0;
-    document.getElementById('current-highscore').innerText = "0";
-            momoSequence();  
+userSeq = [];
+momoSeq = [];
+score = 0;
+document.getElementById('current-highscore').innerText = "0";
+        momoSequence();  
 };
+
