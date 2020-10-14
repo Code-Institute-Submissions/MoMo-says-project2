5d4fe6d040b2d80defbb920dbb2ab50f6178e1d2
QuelleQim <cathrine.karl@gmail.com>
2020-10-14 10:33:25 +0000

    Add code to test-specs.js

---
+++ assets/jasmine-testing/test-specs.js
@@ -0,0 +1,12 @@
+describe("Game", function() {
+
+    describe("winter test", function() {
+        it("should set winter theme", function() {
+        switchWinter();
+        expect(localTheme).toBe("winter");
+        })
+
+    })
+
+
+})
